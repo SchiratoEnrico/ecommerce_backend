deb6e16be4237c8c8a282a1210100584e7152778
Matteo Pescarino <matteo.pescarino@gmail.com>
2026-03-13 12:51:38 +0100

    Creazione models:

---
+++ src/main/java/com/betacom/ecommerce/backend/models/Carrelli.java
@@ -0,0 +1,37 @@
+package com.betacom.ecommerce.backend.models;
+
+import java.util.List;
+
+import jakarta.persistence.CascadeType;
+import jakarta.persistence.Entity;
+import jakarta.persistence.FetchType;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.OneToMany;
+import jakarta.persistence.OneToOne;
+import jakarta.persistence.Table;
+import lombok.Getter;
+import lombok.Setter;
+
+@Setter
+@Getter
+@Entity
+@Table(name="carrelli")
+public class Carrelli {
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	private Integer id;
+	
+	@OneToOne(
+			mappedBy = "utenti",
+			cascade = CascadeType.REMOVE
+			)
+	private Utenti utente;
+	
+	@OneToMany(
+			mappedBy = "utenti",
+			fetch = FetchType.EAGER
+			)
+	private List<Manga> manga;
+}
